ac86255692550fa3347d3140377ea00024c3af88
alex <vurdalackoff@gmail.com>
2024-10-03 18:51:27 +0300

    feat: add some more tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -198,8 +198,9 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / (10 ** pow)) * (10 ** pow);
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -219,8 +220,17 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -238,9 +248,18 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const num = Number(value);
+
+  if (Number.isNaN(num)) {
+    return def;
+  }
+
+  return num;
+  // throw new Error('Not implemented');
 }
+
+// console.log(Number('test'));
 
 module.exports = {
   getRectangleArea,
